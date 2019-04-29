08ff0c029d57616ea6a3a3dba884afc4b10a418e
rahul-murani <rahul@murani.in>
2019-04-29 15:38:12 +0530

    API Added

--- routes/api.php
+++ routes/api.php
@@ -29,13 +29,46 @@ Route::namespace('V1')->prefix('V1')->group(function(){
         Route::post('userList', 'UserController@userList');
         // Create User
         Route::post('/createUser', 'UserController@createUser');
+        // Edit User
+        Route::post('/editUser', 'UserController@editUser');
         // Update User
         Route::post('/updateUser', 'UserController@updateUser');
 
-        
+        // Branch Search List
+        Route::post('searchBranchList', 'BranchController@searchBranchList');
         // Create Branch
         Route::post('/createBranch', 'BranchController@createBranch');
+        // Create Branch
+        Route::post('/editBranch', 'BranchController@editBranch');
         // Update User
         Route::post('/updateBranch', 'BranchController@updateBranch');
+
+        // Academic Year List
+        Route::post('ayList', 'AcademicYearController@academicYearList');
+        // Create Academic Year
+        Route::post('/createAY', 'AcademicYearController@createAY');
+        // Edit Academic Year
+        Route::post('/editAY', 'AcademicYearController@editAY');
+        // Update Academic Year
+        Route::post('/updateAY', 'AcademicYearController@updateAY');
+
+        // Student List
+        Route::post('/studentList', 'StudentController@studentList');
+        // Create Student
+        Route::post('/createStudent', 'StudentController@createStudent');
+        // Edit Student
+        Route::post('/editStudent', 'StudentController@editStudent');
+        // Update Student
+        Route::post('/updateStudent', 'StudentController@updateStudent');
+
+
+        // Fee Offer List
+        Route::post('/foList', 'FeeOfferController@foList');
+        // Create Fee Offer
+        Route::post('/createFO', 'FeeOfferController@createFO');
+        // Edit Fee Offer
+        Route::post('/editFO', 'FeeOfferController@editFO');
+        // Update Fee Offer
+        Route::post('/updateFO', 'FeeOfferController@updateFO');
     });
 });
